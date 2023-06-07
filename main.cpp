a4f3fc78e5de28a1c1ab3824c898363c0439ba75
jaeyjeon <n8720432@naver.com>
2023-06-07 20:44:59 +0900

    add port number error check

--- main.cpp
+++ main.cpp
@@ -12,7 +12,21 @@ int main(int argc, char** argv)
     }
 
     std::string password = argv[2];
+    std::string port_str = argv[1];
+    for(size_t i = 0; i < port_str.size() ; i++)
+    {
+        if (std::isdigit(port_str[i]) == false)
+        {
+            std::cout << "Port numbers can only contain numbers." << std::endl;
+            return 1;
+        }
+    }
     int port = atoi(argv[1]); // 에러 처리 필요??
+    if (port > 65535)
+    {
+        std::cout << "Port numbers cannot be greater than 65535." << std::endl;
+        return 1;
+    }
 
     Server server(password);
     server.startServer(port);
