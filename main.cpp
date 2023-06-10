b185a2ad62093078c6af06009a78efc624ee064b
SeungJoonChoi <tmdwns0845@naver.com>
2023-06-10 09:36:27 +0900

    feat(main): Exception handling using try-catch

--- main.cpp
+++ main.cpp
@@ -5,31 +5,30 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    try
     {
-        std::cout << "usage: ./ircserv <port> <password>" << std::endl;
-        return 1;
-    }
+        if (argc != 3)
+            throw std::runtime_error("usage: ./ircserv <port> <password>");
 
-    std::string password = argv[2];
-    std::string port_str = argv[1];
-    for(size_t i = 0; i < port_str.size() ; i++)
-    {
-        if (std::isdigit(port_str[i]) == false)
+        std::string password = argv[2];
+        std::string port_str = argv[1];
+        for(size_t i = 0; i < port_str.size() ; i++)
         {
-            std::cout << "Port numbers can only contain numbers." << std::endl;
-            return 1;
+            if (std::isdigit(port_str[i]) == false)
+                throw std::runtime_error("Port numbers can only contain numbers.");
         }
+        int port = atoi(argv[1]);
+        if (port > 65535)
+            throw std::runtime_error("Port numbers cannot be greater than 65535.");
+
+        Server server(password);
+        server.startServer(port);
+
+        return 0;
     }
-    int port = atoi(argv[1]); // 에러 처리 필요??
-    if (port > 65535)
+    catch (const std::exception& e)
     {
-        std::cout << "Port numbers cannot be greater than 65535." << std::endl;
+        std::cerr << e.what() << std::endl;
         return 1;
     }
-
-    Server server(password);
-    server.startServer(port);
-
-    return 0;
 }
