77ab2fded4d83c11dcf11a635cc9c3e95cc6e3d9
jaeyjeon <n8720432@naver.com>
2023-06-14 20:06:16 +0900

    fix bot

--- bot/main.cpp
+++ bot/main.cpp
@@ -6,15 +6,16 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 #include <signal.h>
-
-#define JOIN_MSG(channel) "JOIN " + channel + "\r\n";
-#define CHECKIN_MSG(pass) "PASS " + pass + "\r\nNICK TIMEBOT\r\nUSER root root 127.0.0.1 :JJJ\r\n";
+#include <Reply_bot.hpp>
 
 int clnt_sock;
 
 void error_handling(std::string);
 void terminate(int signal);
+std::string get_date();
+int command(std::string& message, std::string ch);
 
 int main(int argc, char *argv[])
 {
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
 	char message[500];
 	std::string msg;
 	std::string ping_msg = "PING ft_irc\r\n";
-	std::string ch_list;
+	std::string ch;
 
 	if (argc != 4)
 	{
@@ -47,28 +48,29 @@ int main(int argc, char *argv[])
 	msg = CHECKIN_MSG(pass);
 	send(clnt_sock, msg.c_str(), msg.size(), 0);
 	memset(&msg, 0, sizeof(msg));
-	result = recv(clnt_sock, message, sizeof(message) - 1, 0);
-	if (result != -1)
-		std::cout << message << std::endl;
-	memset(&message, 0, sizeof(message));
 	// servername을 받아와서 저장해줘야함
 	signal(SIGINT, terminate);
 
-	std::cin >> ch_list;
-	msg = JOIN_MSG(ch_list);
+	std::cin >> ch;
+	msg = JOIN_MSG(ch);
 	send(clnt_sock, msg.c_str(), msg.size(), 0);
 	memset(&msg, 0, sizeof(msg));
-
-	result = recv(clnt_sock, message, sizeof(message) - 1, 0);
-	memset(&message, 0, sizeof(message));
-
+	recv(clnt_sock, message, sizeof(message) - 1, 0);
 	while(1)
 	{
 		result = recv(clnt_sock, message, sizeof(message) - 1, MSG_DONTWAIT);
 		if (result != -1)
 		{
-			std::cout << message << std::endl;
+			msg = message;
+			std::cout << msg << std::endl;
+			result = command(msg, ch);
+			if (result == 1)
+			{
+				close(clnt_sock);
+				exit (0);
+			}
 			memset(&message, 0, sizeof(message));
+			memset(&msg, 0, sizeof(msg));
 		}
 		if (time(NULL) % 50 == 0)
 		{
@@ -96,4 +98,69 @@ void terminate(int signal)
 		close(clnt_sock);
 		exit (0);
 	}
+}
+
+std::string get_date()
+{
+	std::string date;
+	time_t curr_time = time(NULL);
+	struct tm *local_time = NULL;
+	local_time = localtime(&curr_time);
+	date = TIMER(std::to_string(local_time->tm_year + 1900), std::to_string(local_time->tm_mon), \
+	std::to_string(local_time->tm_mday), std::to_string(local_time->tm_mday), \
+	std::to_string(local_time->tm_mday), std::to_string(local_time->tm_mday));
+	return (date);
+}
+
+int command(std::string& message, std::string ch)
+{
+	std::string command = "";
+	std::vector<std::string> params;
+	std::string trailing = "";
+
+	std::stringstream stream;
+	std::string buf;
+	std::string msg;
+
+	message.erase(message.find("\r\n")); // crlf 제거
+	message.erase(0, message.find(' ')); // prefix 제거
+
+	if (message.find(':') != std::string::npos) // _trailing
+	{
+		trailing.append(message.substr(message.find(':') + 1)); //(exclude ':')
+		message.erase(message.find(':')); // message에서 _trailing 제거
+	}
+
+	stream.str(message);
+
+	std::getline(stream, buf, ' ');
+	command.append(buf); // _command
+	buf.clear();
+
+	while(std::getline(stream, buf, ' ')) // _params
+	{
+		params.push_back(buf);
+		buf.clear();
+	}
+
+	///
+	if (!command.compare("PRIVMSG"))
+	{
+		if (trailing[0] == '!')
+		{
+			if (trailing.compare("!time") || trailing.compare("!TIME"))
+			{
+				msg = TIME_MSG(ch, get_date());
+				std::cout << "msg" << std::endl;
+				send(clnt_sock, msg.c_str(), msg.size(), 0);
+			}
+			//...
+		}
+		else
+			return 0;
+	}
+
+	if (!command.compare("KICK"))
+		return 1; //exit
+	return 0;
 }
